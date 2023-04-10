b7b536e36e86594dcc7714a082688354c5ef99d9
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2023-04-09 18:47:32 -0600

    we win

---
+++ src/Components/Router/Router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+export default function Router() {
+    return (
+      
+      <BrowserRouter>
+        <Routes>
+          {/*<Route path="/" element={<Login />} />*/}
+          
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
