9e30553a54145b80795d1d2dee6bc84f0535ceee
marcarballo97 <79873493+marcarballo97@users.noreply.github.com>
2023-04-10 02:00:44 -0600

    perfil plantilla

--- src/Components/Router/Router.jsx
+++ src/Components/Router/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Materias } from "../RutaCuestionario/Materia";
 import Lobby from "../Lobby/Lobby";
 import Nie from "../Registros/RegistroNie";
+import PerfilEstudiante from "../Perfil/Perfil";
 
 
 export default function Router() {
@@ -12,7 +13,8 @@ export default function Router() {
         
           <Route path="/Materia" element={<Materias />} />
           <Route path="/Home" element={<Lobby />} />
-          <Route path="/" element={<Nie />} />
+          <Route path="/Nie" element={<Nie />} />
+          <Route path="/" element={<PerfilEstudiante />} />
           
           
         </Routes>
