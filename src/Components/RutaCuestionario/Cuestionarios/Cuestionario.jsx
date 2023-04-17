c9f5e26ffb1220bb5f9d67fd16fa9ca5ecd7d34e
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2023-04-16 23:32:53 -0600

    Rey

--- src/Components/RutaCuestionario/Cuestionarios/Cuestionario.jsx
+++ src/Components/RutaCuestionario/Cuestionarios/Cuestionario.jsx
@@ -4,6 +4,7 @@ import "../Cuestionarios/cuest.css";
 import { NavbarEstudiante } from "../../NavbarEstudiante";
 import { Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
+import {Footer} from "../../Footer";
 
 export const Cuestionario = () => {
   const [preguntaActual, setPreguntaActual] = useState(0);
@@ -27,31 +28,41 @@ export const Cuestionario = () => {
         setPreguntaActual(preguntaActual + 1);
     }
     }, 500);
-    
   }
 
 
   if(isFinished) return(
     <main>
     <NavbarEstudiante />
-    <section className=" relative overflow-x-auto shadow-md sm:rounded-lg p-12 pt-21">
-      <div className="flex justify-start p-12 text-center  text-white w-full">
-        <Link
-          to="/cuestionario/ver-cuestionario"
-          className="flex justify-start"
-        >
-          <Icon icon="lucide:arrow-big-left" width="35" height="35" />
-        </Link>
-      </div>
-      <div className="flex justify-center bg-opacity-25">
+    <section className="relative overflow-x-auto bg-opacity-50 sm:rounded-lg p-12 pt-32 text-center">
+      <div className="">
       <div className="app">
-      <div className="juego-teerminado">
-        <span className="">Obtuviste {puntaje} de {puntosT} puntos</span>
-        <button className="buttonPuntos" onClick={()=> window.location.href="/temas"}>Volver a cuestionarios</button>
+      <div className="juego-terminado">
+      <h1>
+        <span>H</span>
+        <span>A</span>
+        <span>Z</span>
+        <span className="px-2"> </span>
+        <span>T</span>
+        <span>E</span>
+        <span>R</span>
+        <span>M</span>
+        <span>I</span>
+        <span>N</span>
+        <span>A</span>
+        <span>D</span>
+        <span>O</span>
+        <span>!</span>
+        </h1>
+        <p className="text-3xl mt-12">Obtuviste {puntaje} de {puntosT} puntos</p>
+      </div>
+      <div>
+      <button className="buttonPuntos mt-48 content-end" onClick={()=> window.location.href="/temas"}>Volver a cuestionarios</button>
       </div>
     </div>
       </div>
     </section>
+    <Footer/>
   </main>
     
   )
---
+++ src/Components/RutaCuestionario/Cuestionarios/DescripcionCuest.jsx
@@ -0,0 +1,8 @@
+
+
+export const Descripcioncuest = () =>{
+
+    return(
+        
+    )
+}
